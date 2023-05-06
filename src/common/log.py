dc64c89ead4f26c00859a36cd979ae6577cb2447
pr0xtti <lev.370x@gmail.com>
2023-05-06 16:17:37 +0300

    feature/1: Logging

---
+++ src/common/log.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from loguru import logger
+
+LOG_LEVEL: str = os.getenv('LOG_LEVEL')
+LOGURU_FORMAT: str = os.getenv(
+    'LOGURU_FORMAT',
+    "<level>{level}</level> | " 
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | " 
+    "<cyan>{module}</cyan> | " 
+    "<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
+
+logger.remove()
+error_handler = logger.add(
+    sys.stderr,
+    level=LOG_LEVEL,
+    format=LOGURU_FORMAT
+)
+logger.debug(f"Log level set from env: {LOG_LEVEL}")
