0b0123e94309ad22135205c212c29d3699b0628e
pr0xtti <lev.370x@gmail.com>
2023-05-10 00:00:38 +0300

    feature/1: tools added

--- src/common/tool.py
+++ src/common/tool.py
@@ -1,12 +1,46 @@
 import re
+from urllib.parse import quote_plus
 
 
-def camel_to_snake(name):
-    new_name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+def camel_to_snake(name: str) -> str:
+    """
+    Converts CamelToSnake >> camel_to_snake
+    :param name: CamelString
+    :return: snake_string
+    """
+    new_name: str = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
     return new_name
 
 
-def snake_to_camel(name):
-    new_name = ''.join(word.title() for word in name.split('_'))
+def snake_to_camel(name: str) -> str:
+    """
+    Converts snake_to_camel >> SnakeToCamel
+    :param name: snake_string
+    :return: CamelString
+    """
+    new_name: str = ''.join(word.title() for word in name.split('_'))
     return new_name
+
+
+def make_connection_string(
+        user: str,
+        password: str,
+        server: str,
+        port: str,
+        db: str,
+) -> str:
+    """
+    Makes postgres connections string from attrs
+    :param user:
+    :param password:
+    :param server:
+    :param port:
+    :param db:
+    :return:
+    """
+    return (
+        f"postgresql://{quote_plus(user)}:"
+        f"{quote_plus(password)}@"
+        f"{server}:{port}/{db}"
+    )
 
