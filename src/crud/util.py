f3f338b78b795cd4a8b4b646b1a67875976a09b5
pr0xtti <lev.370x@gmail.com>
2023-06-12 23:57:20 +0300

    feature/1: Added add_item_by_model

--- src/crud/util.py
+++ src/crud/util.py
@@ -3,6 +3,7 @@ from typing import Type
 from sqlalchemy import select
 from sqlalchemy.engine.result import Sequence
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from common.log import logger
 from db.models.base import Base
@@ -24,4 +25,31 @@ def get_items_by_model(
     items: Sequence = db.execute(stmt).scalars().all()
     logger.debug(f"Returning: {len(items)} items of {model}")
     return items
+
+
+def add_item_by_model(
+        db: Session, item: Type[Base], model: Type[Base]
+) -> Type[Base]:
+    """
+    Add record to table with model
+    :param db: Session
+    :param item:
+    :param model: SQLAlchemy model
+    :return:
+    """
+    item_object = model(**item.dict())
+    logger.debug(f"Prepared item: {item_object}")
+    db.add(item_object)
+    try:
+        db.commit()
+    except IntegrityError as e:
+        logger.warning(f"Failed to add item: {item_object}: IntegrityError: {e}")
+        db.rollback()
+    except Exception as e:
+        logger.warning(f"Failed to add item: {e}")
+        db.rollback()
+    else:
+        db.refresh(item_object)
+        logger.debug(f"Inserted item: {item_object}")
+        return item_object
 
