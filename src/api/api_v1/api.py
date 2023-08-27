40c3a882e6428f091305c19d334c5c1d4d6c7cbb
pr0xtti <lev.370x@gmail.com>
2023-08-28 00:23:28 +0300

    feature/1: Cosmetic changes

--- src/api/api_v1/api.py
+++ src/api/api_v1/api.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from src.api.api_v1.endpoints import device
 
 api_router = APIRouter()
-api_router.include_router(router=device.router, prefix="/device", tags=["device"])
+api_router.include_router(router=device.router, prefix="/devices", tags=["devices"])
